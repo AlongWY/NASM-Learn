7a9d765328578ab46eaeddd8267ac2fe69e6e17d
alongwy <15939423778@163.com>
2017-09-05 14:40:05 +0800

    refresh code

--- code-1/helloworld.asm
+++ code-1/helloworld.asm
@@ -14,5 +14,9 @@ _start:
     mov     edx, 13     ; 字符串长度
     mov     ecx, msg    ; 字符串地址
     mov     ebx, 1      ; 写入到标准输出
-    mov     eax, 4      ; 调用SYS_WRITE (中断类型码 4)
-    int     80h
+    mov     eax, 4      ; SYS_WRITE 中断操作数 4
+    int     80h         ; 调用系统中断
+
+    mov     ebx, 0      ; 返回值
+    mov     eax, 1      ; SYS_EXIT 中断操作数 1
+    int     80h         ; 调用系统中断
